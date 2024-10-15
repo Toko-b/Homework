d35745c76a0b9d229d7f8a759a831fe41cf68cb9
good guy <tokobuskivadze@gmail.com>
2024-10-15 23:06:19 +0400

    day 3

---
+++ level3/class/lesson3.py
@@ -0,0 +1,10 @@
+name = "Tornike"
+last_name = "Buskivadze"
+age = 13
+country = "Georgia"
+home_town = "rustavi"
+favorite_car = "ferrari"
+favorite_hobby = "swimming"
+favorite_color = "green"
+
+print ("Hello my name is " + name + " and my last name is " + last_name + ". i live at " + country + ", my home town is " + home_town + ". my favorite car is " + favorite_car + " And my hobby is " + favorite_hobby + ", my favorite color is " + favorite_color + "." )
