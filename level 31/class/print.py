65ce8160e8f955971a8916b0673f8e18f1186190
good guy <tokobuskivadze@gmail.com>
2025-01-25 21:11:22 +0400

    level 31

--- level 31/class/print.py
+++ level 31/class/print.py
@@ -1,3 +1,6 @@
-
+name = "Toko"
+last_name = "Buskivadze"
+age = 13
+print(f"hello my name is {name}, my last name is {last_name} and i am {age} years old")
 
 print("hello my name is {0} and my last name is {1} and i am {2} years old".format("Toko", "Buskivadze", 13))
