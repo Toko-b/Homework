37b328daae878929cf360ee4fd8680d5273c27bc
good guy <tokobuskivadze@gmail.com>
2025-01-25 21:09:19 +0400

    level 31

---
+++ level 31/class/print.py
@@ -0,0 +1,3 @@
+
+
+print("hello my name is {0} and my last name is {1} and i am {2} years old".format("Toko", "Buskivadze", 13))
---
+++ level 31/home/hw.py
@@ -0,0 +1,36 @@
+# task 1 it was sololearn and i did it
+
+# task 2
+
+name = "Alice"
+place = "Wonderland"
+
+format_string = f"Hello, {name}. Welcome to {place}"
+print(format_string)
+
+formatted_string = "Hello, {0}. Welcome to {1}".format("Alice", "Wonderland")
+print(formatted_string)
+
+# task 3
+
+fruits = ["apple", "banana", "cherry"]
+fruit_string = ", ".join(fruits)
+print(fruit_string)
+
+# task 4
+
+sentenace =  "The quick brown fox jumps over the lazy dog."
+word_list = sentenace.split(" ")
+print(word_list)
+
+# task 5
+
+mixed_case = "PyThOn Is AwEsOmE!"
+lowercase_string = mixed_case.lower()
+print(lowercase_string)
+
+# task 6
+
+greeting = "good morning"
+uppercase_greeting = greeting.upper()
+print(uppercase_greeting)
