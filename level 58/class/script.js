6aeb165c8d981a5b5c03a4411d6f01e52b0fd6f6
good guy <tokobuskivadze@gmail.com>
2025-04-28 22:53:20 +0400

    level 57

--- level 58/class/script.js
+++ level 58/class/script.js
@@ -1,20 +1,20 @@
-// let balance = prompt("Your balance:");
-// let withdrawal = prompt("How much do you want to withdraw? : ");
+let balance = prompt("Your balance:");
+let withdrawal = prompt("How much do you want to withdraw? : ");
 
-// balance -= withdrawal;
-// console.log(`after the withdrawal you have $${balance} left in your bank account`);
-
-
-// let country = prompt("Please enter a country: ");
-// let capital = prompt("Please enter that countries capital: ");
-
-// console.log(`Country: ${country}, Capital: ${capital}`);
+balance -= withdrawal;
+console.log(`after the withdrawal you have $${balance} left in your bank account`);
 
 
-// let name = prompt("Please entert your name: ");
+let country = prompt("Please enter a country: ");
+let capital = prompt("Please enter that countries capital: ");
 
-// console.log("Welcome " + name);
-// //or
-// console.log(`Welcome ${name}`);
+console.log(`Country: ${country}, Capital: ${capital}`);
+
+
+let name = prompt("Please entert your name: ");
+
+console.log("Welcome " + name);
+//or
+console.log(`Welcome ${name}`);
 
 
