a0966a46d4b37e32edf020eaf11c17b542c28a05
good guy <tokobuskivadze@gmail.com>
2025-05-10 21:25:23 +0400

    level 61

---
+++ level 61/class/script.js
@@ -0,0 +1,16 @@
+for(let i = 2; i <= 50; i += 2){
+    console.log(i);
+}
+
+for(let x = 0; x <= 200; x++){
+    if (x % 2 === 0){
+        console.log(`ლუწი: ${x}`);
+    }
+    else{
+        console.log(`კენტი ${x}`);
+    }
+}
+
+for(let v = 0; v < 3; v++){
+    console.log("GOA IS THE BEST");
+}
---
+++ level 61/home/script.js
@@ -0,0 +1,36 @@
+// let sum = 0;
+
+// for(let i = 1; i < 11; i++){
+//     sum += i;
+// }
+
+// console.log(sum);
+
+
+// for(let x = 1; x < 11; x++){
+//     console.log(x**2);
+// }
+
+// let input = prompt("enter a word");
+
+// for(let i = 0; i < input.length; i++){
+//     console.log(input[i])
+// }
+
+let a = "Fizz";
+let b = "Buzz";
+
+let num = Number(prompt("enter a number: "))
+
+if(num % 3 === 0 && num % 5 === 0){
+    console.log(a + b);
+}
+else if(num % 3 === 0){
+    console.log(a);
+}
+else if(num % 5 === 0){
+    console.log(b);
+}
+else{
+    console.log(num);
+}
